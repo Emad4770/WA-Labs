98617091170caa40324a956e531d1b2c7f90d0c4
Emad4770 <emad47.n7@gmail.com>
2024-08-15 20:22:55 +0200

    Update API endpoints for film management

--- Lab03/dao.mjs
+++ Lab03/dao.mjs
@@ -58,128 +58,126 @@ export default function FilmDao() {
                 }
             })
         })
-
-
-
     }
-}
 
 
-export function getAllFilms() {
-    return new Promise((resolve, reject) => {
-        const sql = `SELECT id, title FROM films`;
-        db.all(sql, (err, rows) => {
-            if (err) reject(err);
-            else if (rows.length == 0) {
-                resolve({
-                    error: "There are no films in the database, try again later!",
-                });
-            } else {
-                const fList = rowsToFilm(rows);
-                resolve(fList);
-            }
-        });
-    });
-}
 
-export function getFavoriteFilms() {
-    return new Promise((resolve, reject) => {
-        const sql = `SELECT id, title, rating ,watchDate, userId FROM films f
-        WHERE f.isFavorite = 1`;
+    // export function getAllFilms() {
+    //     return new Promise((resolve, reject) => {
+    //         const sql = `SELECT id, title FROM films`;
+    //         db.all(sql, (err, rows) => {
+    //             if (err) reject(err);
+    //             else if (rows.length == 0) {
+    //                 resolve({
+    //                     error: "There are no films in the database, try again later!",
+    //                 });
+    //             } else {
+    //                 const fList = rowsToFilm(rows);
+    //                 resolve(fList);
+    //             }
+    //         });
+    //     });
+    // }
 
-        db.all(sql, (err, rows) => {
-            if (err) reject(err);
-            if (rows.length == 0) {
-                resolve({ error: "No favorite films found!" });
-            } else {
-                const films = rows.map((row) => {
-                    return new Film(
-                        row.id,
-                        row.title,
-                        1,
-                        row.watchDate,
-                        row.rating,
-                        row.userId
-                    );
-                });
-                resolve(films);
-            }
-        });
-    });
-}
+    // export function getFavoriteFilms() {
+    //     return new Promise((resolve, reject) => {
+    //         const sql = `SELECT id, title, rating ,watchDate, userId FROM films f
+    //         WHERE f.isFavorite = 1`;
 
-export function getTopRated() {
-    return new Promise((resolve, reject) => {
-        const sql = `SELECT id, title, isFavorite, watchDate, rating, userId FROM films
-        WHERE rating = 5`;
-        db.all(sql, (err, rows) => {
-            if (err) reject(err);
-            else if (rows.length == 0) {
-                resolve({ error: "No movies found!" });
-            } else {
-                const films = rowsToFilm(rows);
-                resolve(films);
-            }
-        });
-    });
-}
+    //         db.all(sql, (err, rows) => {
+    //             if (err) reject(err);
+    //             if (rows.length == 0) {
+    //                 resolve({ error: "No favorite films found!" });
+    //             } else {
+    //                 const films = rows.map((row) => {
+    //                     return new Film(
+    //                         row.id,
+    //                         row.title,
+    //                         1,
+    //                         row.watchDate,
+    //                         row.rating,
+    //                         row.userId
+    //                     );
+    //                 });
+    //                 resolve(films);
+    //             }
+    //         });
+    //     });
+    // }
 
-export function getLatestFilms(currentDate, lastMonth) {
-    return new Promise((resolve, reject) => {
-        const sql = `SELECT * FROM films
-        WHERE watchDate<= DATE(?) and watchDate >= DATE(?)`;
-        db.all(
-            sql,
-            [currentDate.toISOString(), lastMonth.toISOString()],
-            (err, rows) => {
-                if (err) reject(err);
-                else if (rows.length == 0) {
-                    resolve({ error: "No films have been watched in the past month!" });
-                } else {
-                    resolve(rowsToFilm(rows));
-                }
-            }
-        );
-    });
-}
+    // export function getTopRated() {
+    //     return new Promise((resolve, reject) => {
+    //         const sql = `SELECT id, title, isFavorite, watchDate, rating, userId FROM films
+    //         WHERE rating = 5`;
+    //         db.all(sql, (err, rows) => {
+    //             if (err) reject(err);
+    //             else if (rows.length == 0) {
+    //                 resolve({ error: "No movies found!" });
+    //             } else {
+    //                 const films = rowsToFilm(rows);
+    //                 resolve(films);
+    //             }
+    //         });
+    //     });
+    // }
 
-export function getUnseenFilms() {
-    return new Promise((resolve, reject) => {
-        const sql = `SELECT * FROM films
-        WHERE watchDate IS NULL`;
-        db.all(sql, (err, rows) => {
-            if (err) reject(err);
-            else if (rows.length == 0) {
-                resolve({ error: "No unseen films found!" });
-            } else {
-                resolve(rowsToFilm(rows));
-            }
-        });
-    });
-}
+    // export function getLatestFilms(currentDate, lastMonth) {
+    //     return new Promise((resolve, reject) => {
+    //         const sql = `SELECT * FROM films
+    //         WHERE watchDate<= DATE(?) and watchDate >= DATE(?)`;
+    //         db.all(
+    //             sql,
+    //             [currentDate.toISOString(), lastMonth.toISOString()],
+    //             (err, rows) => {
+    //                 if (err) reject(err);
+    //                 else if (rows.length == 0) {
+    //                     resolve({ error: "No films have been watched in the past month!" });
+    //                 } else {
+    //                     resolve(rowsToFilm(rows));
+    //                 }
+    //             }
+    //         );
+    //     });
+    // }
 
-export function getFilm(id) {
-    return new Promise((resolve, reject) => {
-        const sql = `SELECT * FROM films
+    // export function getUnseenFilms() {
+    //     return new Promise((resolve, reject) => {
+    //         const sql = `SELECT * FROM films
+    //         WHERE watchDate IS NULL`;
+    //         db.all(sql, (err, rows) => {
+    //             if (err) reject(err);
+    //             else if (rows.length == 0) {
+    //                 resolve({ error: "No unseen films found!" });
+    //             } else {
+    //                 resolve(rowsToFilm(rows));
+    //             }
+    //         });
+    //     });
+    // }
+
+    this.getFilm = (id) => {
+        return new Promise((resolve, reject) => {
+            const sql = `SELECT * FROM films
         WHERE id = ?`;
-        db.get(sql, [id], (err, row) => {
-            if (err) reject(err);
-            else if (row === undefined) {
-                resolve({ error: "No films found with this id, try again!" });
-            } else {
-                resolve(
-                    new Film(
-                        row.id,
-                        row.title,
-                        row.isFavorite,
-                        row.watchDate,
-                        row.rating,
-                        row.userId
-                    )
-                );
-            }
+            db.get(sql, [id], (err, row) => {
+                if (err) reject(err);
+                else if (row === undefined) {
+                    resolve({ error: "No films found with this id, try again!" });
+                } else {
+                    resolve(
+                        new Film(
+                            row.id,
+                            row.title,
+                            row.isFavorite,
+                            row.watchDate,
+                            row.rating,
+                            row.userId
+                        )
+                    );
+                }
+            });
         });
-    });
+    }
 }
 
 export function addNewFilm(film) {
